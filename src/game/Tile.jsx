8adfd3f030a201855a9406e22aae200e50a9a9bf
Zoltán Mihályfi <mihalyfi.zoltan@gmail.com>
2023-11-28 18:38:09 +0100

    fix function

--- src/game/Tile.jsx
+++ src/game/Tile.jsx
@@ -131,7 +131,7 @@ const Tile = ({
       className={className}
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
-      onClick={() => myclick()}
+      onClick={() => handleTileClick()}
 
       // onTouchStart={handleTouchStart
       // onTouchEnd={handleTouchEnd}
