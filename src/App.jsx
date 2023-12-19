279d98c04bd5020ddd90941e7b6f0243d70d6716
Zoltán Mihályfi <mihalyfi.zoltan@gmail.com>
2023-12-19 16:46:58 +0100

    add ga for testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import styles from "./app.module.css";
 import UseAnalyticsEventTracker from "./UseAnalyticsEventTracker";
 
 import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-RSMM8752NE";
+const TRACKING_ID = "G-1EJYGXJ5D3";
+// const TRACKING_ID = "G-RSMM8752NE";
 ReactGA.initialize(TRACKING_ID);
 
 function App({ setShowAbout }) {
